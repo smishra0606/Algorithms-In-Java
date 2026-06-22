dc67a8f2adb65cad38778a7a560fa1c0921be0f5
SHIVEN MISHRA <shivenmishra0606@gmail.com>
2026-06-22 11:52:22 +0530

    Refurbishing Basics Of Programming

---
+++ JAVA/Basics.java
@@ -0,0 +1,75 @@
+package JAVA;
+
+//Data Types in Java
+//int-> 4 bytes
+//float-> 4 bytes
+//double-> 8 bytes
+//char-> 2 bytes
+//long-> 8 bytes
+//boolean-> 1 byte
+
+
+import java.util.Scanner;
+
+public class Basics{
+    public static void main(String[] args) {
+
+        //basic input and output
+        // int age=5;
+        // int number=0;
+        // Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter your age:");
+        // age=sc.nextInt();
+        // System.out.println("Enter a number:");
+        // number=sc.nextInt();
+        // if(age>=18 && number>10){
+        //     System.out.println("The Test will be conducted");
+        // }
+        // else{
+        //     System.out.println("The Test will not be conducted");
+        // }
+
+
+        //Switch case
+        // char grade='A';
+        // switch(grade){
+        //     case 'A':
+        //         System.out.println("You have an A grade");
+        //     case 'B':
+        //         System.out.println("You have a B grade");
+        //     case 'C':
+        //         System.out.println("You have a C grade");
+        //     default:
+        //         System.out.println("Invalid grade");
+        // }
+        
+
+        //ternary operator
+        // int x=10;
+        // int y=20;
+        // System.out.println(x>y ? x : y);
+
+        //Loops
+
+        //for Loop
+        // for(int i=0;i<10;i++){
+        //     System.out.println(i);
+        // }
+
+        //while Loop
+        // int i=0;
+        // while(i<5){
+        //         System.out.println(i);
+        //         i++;
+        // }
+
+        //Do While Loop
+        // int i=0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // while(i<5);
+    }
+
+}
